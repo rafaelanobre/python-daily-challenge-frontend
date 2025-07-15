3e335e1baafe3ae7bb975faff30893ceb627e0b2
Rafaela Nobre <raafanobre@gmail.com>
2025-07-14 23:27:36 -0300

    :sparkles: Feat: Create routes and auth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { ClerkProviderWithRoutes } from './auth/ClerkProviderWithRoutes.jsx';
+import ClerkProviderWithRoutes from './auth/ClerkProviderWithRoutes.jsx';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './layout/Layout.jsx';
 import { ChallengeGenerator } from './challenge/ChallengeGenerator.jsx';
@@ -10,7 +10,14 @@ function App() {
   return (
     <ClerkProviderWithRoutes>
       <Routes>
-        <Route></Route>
+        <Route path="/sign-in/*" element={ <AuthenticationPage /> } />
+        <Route path="/sign-up" element={ <AuthenticationPage /> } />
+
+        <Route element={ <Layout /> }>
+            <Route path="/" element={ <ChallengeGenerator /> } />
+            <Route path="/history" element={ <HistoryPanel /> } />
+        </Route>
+
       </Routes>
     </ClerkProviderWithRoutes>
   );
